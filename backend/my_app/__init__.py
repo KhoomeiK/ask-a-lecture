1e6664f91b1bd478e26c0ca3f08b07ff6e73f7ef
Gabriel Ting <gabrielfting@gmail.com>
2020-09-19 16:43:01 -0500

    Add CORs from frontend

--- backend/my_app/__init__.py
+++ backend/my_app/__init__.py
@@ -1,8 +1,11 @@
 import os, config
 from flask import Flask, session, request
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
+
+cors = CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}})
 
 app.config.from_pyfile('../config.py') 
 db = SQLAlchemy(app)
