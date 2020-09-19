3668b1ddcc1a7bb1850c4fc27262cc6a1f78375e
pearlli98 <pearlli@mit.edu>
2020-09-19 17:36:43 -0400

    changes post/get fields

--- backend/my_app/views.py
+++ backend/my_app/views.py
@@ -1,25 +1,33 @@
 # Views at the end of Workshop 2
 from my_app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, jsonify
 from my_app.parse import parse_file
+
+@app.route("/")
+def hi():
+    return "hello world"
 
 @app.route("/api/class", methods=["POST"])
 def add_lecture():
     if request.method == "POST":
         print(request)
         post_info = request.get_json()
-        lectureId = post_info.get("lectureNumber")
+        classId = post_info.get("classId")
         lectureTitle = post_info.get("lectureTitle")
+        lectureNumber = post_info.get("lectureNumber")
         script = post_info.get("transcript")
         link = post_info.get("videoLink")
         parsed_script = parse_file(script)
-    return redirect("/")
+        print(lectureTitle)
+    return jsonify({"response": "added lecture"})
 
-@app.route("/api/search", methods=["POST"])
+@app.route("/api/search", methods=["GET"])
 def search():
-    if request.method == "POST":
+    if request.method == "GET":
         print(request)
         post_info = request.get_json()
-        lectureId = post_info.get("lectureNumber")
+        classId = post_info.get("classId")
+        lectureNumber = post_info.get("lectureNumber")
         question = post_info.get("question")
-    return redirect("/")
+        timeStamps = []
+    return jsonify(timeStamps)
