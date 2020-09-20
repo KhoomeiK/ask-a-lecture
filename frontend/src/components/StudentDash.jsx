117f8070a2137167fe5bb505e98e813a7e7248bc
pearlli98 <pearlli@mit.edu>
2020-09-19 22:50:06 -0400

    student dash

--- frontend/src/components/StudentDash.jsx
+++ frontend/src/components/StudentDash.jsx
@@ -1,9 +1,82 @@
-import React, {useCallback} from 'react'
-import {useDropzone} from 'react-dropzone'
-import {classes} from '../utils'
+/** @jsx jsx */
+import React, { useState } from 'react'
+
+import {
+    Link
+} from "react-router-dom";
+import { jsx, 
+    Heading, 
+    Button,     
+    Label,
+    Input,
+    Select,
+    Textarea,
+    Radio,
+    Checkbox,
+    Box,
+    Flex,
+    Slider,} from 'theme-ui'
+
+import { StyledDropzone } from './Dropzone'
+
+import { classes, postLecture, popToast } from '../utils'
+
+import { Collapse, CardBody, Card } from 'reactstrap';
 
 export const StudentDash = (props) => {
+    // Strip out stuff for nav
+    const {to, staticContext, activeClassName, ...rest} = props
 
-    return (<>hi</>)
-    
+    // const [loading, setLoading] = useState(false)
+    const [text, setText] = useState("")
+    const [isOpen, setIsOpen] = useState(false);
+    const toggle = () => setIsOpen(!isOpen);
+
+    return (
+        <React.Fragment>
+            <header
+                sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    variant: 'styles.header',
+                }}
+                className="header">
+                <Heading>Ask-A-Lecture</Heading>
+                <div sx={{ mx: 'auto' }} />
+                <Link {...rest} to="/professor"
+                    sx={{
+                        color: 'inherit',
+                        '&.active': {
+                            color: 'primary',
+                        },
+                        marginRight: '10px'
+                    }}>Professor View</Link> 
+
+            </header>
+            <div className="dashboard">
+                {/* <StyledDropzone setText={setText} setLoading={setLoading} /> */}
+                <Box
+                    style={{marginTop: '30px'}}>
+                    {/* onSubmit={e => uploadTranscript(e, text)} */}
+                    {/* <Button onClick={toggle} style={{ marginBottom: '1rem' }}>18.01</Button> */}
+                    <Button onClick={toggle} style={{ marginBottom: '1rem' }}>18.01</Button>
+                    <Collapse isOpen={isOpen}>
+                        <Card>
+                        <CardBody>
+                        <Link {...rest} to="/search"
+                                    sx={{
+                                        color: 'inherit',
+                                        '&.active': {
+                                            color: 'primary',
+                                        },
+                                        marginRight: '10px'
+                                    }}>lecture 1</Link> 
+                        </CardBody>
+                        </Card>
+                    </Collapse>
+                </Box>
+               
+            </div>
+        </React.Fragment>
+    )
 }
